b5a74adb2caf66c41e7b7c82e1fb679d8f551f9e
Jared Strohschein <45718292+jstrohschein@users.noreply.github.com>
2020-06-10 23:16:17 -0400

    Task 1 and 2 complete

--- index.js
+++ index.js
@@ -40,9 +40,68 @@ class Airplane {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
+
+
+
+
+
 class Person {
 
-}
+  constructor(name, age) {
+
+    this.name = name;
+
+    this.age = age;
+
+    this.stomach = [];
+  }
+
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      return this.stomach.push(someFood);
+    }
+
+    else {
+     return console.log ('Stomach is full');
+    }
+  }
+
+  poop () {
+    this.stomach = [];
+  }
+
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
+
+};
+
+const me = new Person('Jared', 30);
+
+
+//eat test
+// console.log(me);
+// me.eat ('sushi');
+// console.log(me.stomach);
+
+
+//poop test
+// for (let i = 0; i < 11; i++) {
+//   me.eat ('sushi');
+// };
+// console.log(me.stomach);
+// me.poop();
+// console.log(me.stomach);
+
+
+//toString test
+// console.log(me.toString());
+
+
+
+
+
+
 
 /*
   TASK 2
@@ -58,9 +117,66 @@ class Person {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
+
+
+
+
+
 class Car {
 
+  constructor(model, milesPerGallon) {
+
+    this.model = model;
+  
+    this.milesPerGallon = milesPerGallon;
+  
+    this.tank = 0;
+  
+    this.odometer = 0;
+  }
+  
+  fill(gallons) {
+      this.tank += gallons;
+  }
+  
+  drive(distance) {
+  
+    let fuel = this.tank - distance / this.milesPerGallon;
+  
+    if (fuel > 0){
+      this.odometer += distance;
+      this.tank = fuel;
+    }
+  
+    else {
+      this.odometer += this.milesPerGallon * this.tank;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+      
+  }
+  
 }
+
+
+// car constructor test 
+// const whip = new Car('Mustang', 10);
+// console.log(whip);
+
+// fill test
+// whip.fill(25);
+// console.log(whip);
+
+// drive test w/o running out of fuel
+// whip.drive(10);
+// console.log(whip);
+
+// drive test running out of fuel
+// console.log(whip.drive(400));
+
+
+
+
 
 /*
   TASK 3
@@ -74,9 +190,46 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
+
+
+
+
+
 class Lambdasian {
 
-}
+  constructor(student) {
+    this.name = student.name;
+    this.age = student.age;
+    this.location = student.location;
+  }
+
+  speak(){
+    return `Hello, my name is ${this.name}, I am from ${this.location}`;
+  }
+
+};
+
+const aang = new Lambdasian ({
+  name: 'Aang',
+  age: 112,
+  location: 'The Southern Air Temple',
+});
+
+
+
+//constructor test
+//console.log(aang);
+
+//speak test
+//console.log(aang.speak());
+
+
+
+
+
+
+
+
 
 /*
   TASK 4
@@ -92,9 +245,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
+
+
+
+
+
 class Instructor {
 
 }
+
+
+
+
+
 
 /*
   TASK 5
@@ -111,9 +274,19 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
+
+
+
+
+
 class Student {
 
 }
+
+
+
+
+
 
 /*
   TASK 6
@@ -128,9 +301,19 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
+
+
+
+
+
 class ProjectManager {
 
 }
+
+
+
+
+
 
 /*
   STRETCH PROBLEM (no tests!)
@@ -140,6 +323,15 @@ class ProjectManager {
       + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
+
+
+
+
+
+
+
+
+
 
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
